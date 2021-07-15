540ab072c62bbd51cef4d2632c2df50e9715750c
JefersonEGomes <jefersonsportsempre@gmail.com>
2021-07-15 16:44:06 -0300

    adcionando segundo exemplo

---
+++ lifecycle/src/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+class App extends Component{
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  
+    <App />
+  ,
+  document.getElementById('root')
+);
+
+reportWebVitals();
